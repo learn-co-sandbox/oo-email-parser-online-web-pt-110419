cc6c65ec7cd874a402692e9bd60b664de3a944ef
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-01-10 19:07:57 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,7 +3,7 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-class EmailParser
+class EmailAddressParser
 
   attr_accessor :emails
   
@@ -20,9 +20,9 @@ class EmailParser
 end
 
 emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
-parser1 = EmailParser.new(emails1)
+parser1 = EmailAddressParser.new(emails1)
 parser1.parse 
 
 emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
-parser2 = EmailParser.new(emails2)
+parser2 = EmailAddressParser.new(emails2)
 parser2.parse
