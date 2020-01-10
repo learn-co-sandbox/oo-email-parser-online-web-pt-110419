f5b24265aabd327d94b6808e21be40977c7239f8
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-01-10 19:00:20 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,11 +9,16 @@ class EmailParser
   def  initialize(emails)
     @email = emails
   end
-
+  
   def parse
-    email_array = @email.split(/[, ]/).uniq
-    email_array.reject! {|element| element.empty?}
-    email_array
+     emails.delete(',').split.uniq
   end
 
 end
+  #def parse
+   # email_array = @email.split(/[, ]/).uniq
+   # email_array.reject! {|element| element.empty?}
+   # email_array
+  #end
+
+#end
